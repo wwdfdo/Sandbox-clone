3301e26676b2c01a9f2d3815e2f8599408ac8e29
wwdfdo <wwdfdo@gmail.com>
2022-05-05 03:03:00 +0530

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      color: {
+        alphaBox: "rgba(124, 123, 123, 0.24706)",
+      },
+      rotate: {
+        dragon: "rotateY(180deg)",
+      },
+
       backgroundImage: {
         alphaBg: "url('/images/Alpha-coming-bg.png')",
+        bgAvatar: "url('/images/sandbox-avatar.png')",
+        landBg: "url('/images/land-bg.png')",
+        sandBg: "url('/images/sand-bg.png')",
+        nftBg: "url('/images/bg-nft.png')",
       },
     },
   },
